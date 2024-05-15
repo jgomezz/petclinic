55293a30fc26e3df95319f6118e585b496cf10fd
jgomezz <jgomezz@gmail.com>
2024-05-14 23:26:55 -0500

    update spring-boot 3.1.11

--- src/test/java/com/tecsup/petclinic/services/PetServiceTest.java
+++ src/test/java/com/tecsup/petclinic/services/PetServiceTest.java
@@ -1,18 +1,15 @@
 package com.tecsup.petclinic.services;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
-import org.junit.jupiter.api.Test;
-
 import com.tecsup.petclinic.entities.Pet;
 import com.tecsup.petclinic.exception.PetNotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
